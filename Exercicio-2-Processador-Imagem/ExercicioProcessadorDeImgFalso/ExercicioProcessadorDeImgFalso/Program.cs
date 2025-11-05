96a6e12952e4841070985b724906e26adc981a17
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-11-05 20:23:23 -0300

    Add files via upload

---
+++ Exercicio-2-Processador-Imagem/ExercicioProcessadorDeImgFalso/ExercicioProcessadorDeImgFalso/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProcessadorDeImgFalso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int totalPixels = 1000; //10;  // Tamanho da imagem (4K)
+
+            Console.WriteLine("Exercício 2 (Média): Processador de Imagem Falso");
+            Console.WriteLine("Iniciando processamento de imagem...");
+
+            Stopwatch sw1 = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+
+            // Usando For - Sequencial 
+            sw1.Start();
+            for (int i = 0; i < totalPixels; i++)
+            {
+                ProcessarPixel(i);
+                Console.WriteLine($"Pixel processado: {i}");
+            }
+            sw1.Stop();
+
+            // Usando Parallel.For - Paralelo
+            sw2.Start();
+            Parallel.For(0, totalPixels, (i) =>
+            {
+                ProcessarPixel(totalPixels);
+            });
+            sw2.Stop();
+
+            Console.WriteLine($"Tempo do For: {sw1.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Tempo do Parallel.For: {sw2.ElapsedMilliseconds} ms");
+
+            Console.ReadKey();
+        }
+
+        public static void ProcessarPixel(int pixelIndex)
+        {
+            // Crie um método void que simula trabalho pesado
+            System.Threading.Thread.Sleep(1); // Simula trabalho pesado
+        }         
+
+    }
+}
---
+++ Exercicio-2-Processador-Imagem/ExercicioProcessadorDeImgFalso/ExercicioProcessadorDeImgFalso/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ExercicioProcessadorDeImgFalso")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExercicioProcessadorDeImgFalso")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("7b6735e2-3b7d-4d3a-be81-d1c4d1a091d5")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
