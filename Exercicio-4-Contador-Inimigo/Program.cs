df582efd88aa166bfe4fbd6858fa8bea8ac8e65e
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-11-05 20:34:11 -0300

    Add files via upload

---
+++ Exercicio-4-Contador-Inimigo/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ExercicioContadorInimigo
+{
+    // --- Classes do Jogo (Nao modificar ) ---
+
+    // Objetivo: Identificar e corrigir uma race condition em um contexto de jogo usando Interlocked.
+
+    public struct Vector2
+    {
+        public float X, Y;
+        public Vector2(float x, float y) { X = x; Y = y; }
+
+        public static float Distance(Vector2 a, Vector2 b)
+        {
+            float dx = a.X - b.X;
+            float dy = a.Y - b.Y;
+            return (float)Math.Sqrt(dx*dx + dy*dy);
+        }
+    }
+
+    public class Inimigo
+    {
+        public Vector2 Posicao;
+        public Inimigo(Random rand)
+        {
+            Posicao = new Vector2((float)rand.NextDouble() * 1000,
+            (float)rand.NextDouble() * 1000);
+        }
+
+    }
+    // --- Fim das Classes do Jogo ---
+
+
+    // --- Metodo Principal ---
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 4 (Média/Esqueleto): Contador de Inimigos em Alcance
+            Console.WriteLine("Exercício 4 - Contador de Inimigos em Alcance ");
+
+            Random rand = new Random();
+            List<Inimigo> todosInimigos = new List<Inimigo>();
+
+            for (int i = 0; i < 1000000; i++)
+            { // 1 Milhao de inimigos
+                todosInimigos.Add(new Inimigo(rand));
+            }
+
+             Vector2 posJogador = new Vector2(500, 500);
+             float raioAgro = 50.0f;
+
+             int inimigosProximos = 0;
+
+            // --- TAREFA : O CODIGO ABAIXO ESTA ERRADO ---
+            // Ele sofre de uma race condition e NUNCA reporta o numero correto.
+            // 1. Rode o codigo e veja o resultado ( sera baixo e inconsistente ). = Resposta: O codigo ainda n foi corrigoido, assim temos um valor baixo ali por volta do 7600.
+            // 2. Corrija a linha " inimigosProximos ++;" usando Interlocked.
+
+            Parallel.ForEach(todosInimigos, (inimigo) =>
+             {
+                  if (Vector2.Distance(inimigo.Posicao, posJogador) < raioAgro) {
+
+                     // Adicionar o InterLocked
+                     Interlocked.Increment(ref inimigosProximos); // Com ele adicionado os valoes são acima de 7800!!
+                     // inimigosProximos++; // <--- ESTA LINHA ESTA ERRADA !
+                  }
+             });
+
+              Console.WriteLine($" Total de inimigos proximos: {inimigosProximos}");
+
+              // (O numero correto , sequencial , e geralmente por volta de 7800 - 7900)
+        }
+    }
+    
+}
---
+++ Exercicio-5-Atualizacao-Bonus-Buffs/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ExercicioAtualizacaoBonusBuffs
+{
+
+    // Exercício 5 (Baixa/Esqueleto): Atualização de Bônus (Buffs)
+
+    // Objetivo: Usar Parallel.ForEach para acelerar uma tarefa "embaraçosamente paralela"(embarrassingly parallel).
+
+    // --- Classe do Jogo (Nao modificar ) ---
+    public class Buff {
+
+        public string Nome;
+        public float TempoRestante;
+        public bool Ativo;
+
+        public Buff(Random rand) {
+
+            Nome = " Buff_ " + rand.Next(100);
+            TempoRestante = (float)rand.NextDouble() * 30.0f; // 0 a 30s
+            Ativo = true;
+        }
+
+
+        // Metodo que " gasta " o tempo do buff
+        public void Update(float deltaTime)
+        {
+
+            if (!Ativo) return;
+            TempoRestante -= deltaTime;
+            if (TempoRestante <= 0)
+            {
+                Ativo = false;
+            }
+        }
+    }
+    //// --- Fim da Classe do Jogo ---
+
+
+    // --- Metodo Principal ---
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+            List<Buff> todosBuffs = new List<Buff>();
+
+            int totalBuffs = 5000000; // 5 Milhoes de buffs
+            for (int i = 0; i < totalBuffs; i++)
+            {
+                todosBuffs.Add(new Buff(rand));
+            }
+            
+            float deltaTime = 0.1f; // Simulando 100 ms de tick
+            Stopwatch sw = new Stopwatch();
+
+            // --- TAREFA 1: Rodar Sequencial ---
+            Console.WriteLine(" Iniciando update sequencial (lento)...");
+            sw.Start();
+            foreach (var buff in todosBuffs)
+            {
+                buff.Update(deltaTime);
+            }
+            sw.Stop();
+            Console.WriteLine($" Sequencial: { sw.ElapsedMilliseconds } ms");
+
+
+            // --- TAREFA 2: Rodar Paralelo ---
+            Console.WriteLine("\nIniciando update paralelo (rapido)...");
+            sw.Restart();
+            Parallel.ForEach(todosBuffs, (buff) =>
+            {
+                buff.Update(deltaTime);
+            });
+            sw.Stop();
+            Console.WriteLine($" Paralelo: {sw.ElapsedMilliseconds} ms");
+
+            //
+            // --- COLOQUE SEU CODIGO Parallel . ForEach AQUI ---
+            // Substitua o loop ’foreach ’ acima por um ’Parallel.ForEach ’
+            //
+
+            // PARA NAO EU ESQUECER 
+            // O primeiro parametro é todos da coleção enquanto o segundo é somente cada elemento de forma individual.
+
+            Console.ReadKey();
+        }
+    }
+}
